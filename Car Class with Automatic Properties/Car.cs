5f356c27b6ef33e5f0e9eef438640ccfba6aa0d8
Abdelrahman A.Khalaf <103902707+justOcta@users.noreply.github.com>
2025-02-12 19:49:20 +0200

    Assignment (3)

---
+++ Car Class with Automatic Properties/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Class_with_Automatic_Properties;
+internal class Car
+{
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public int Mileage { get; set; }
+
+    public void Drive(int distance)
+    {
+        Mileage+= distance;
+    }
+
+    public int Age
+    { 
+        get 
+        {
+            return DateTime.Now.Year-Year;
+        }
+    }
+}
---
+++ Car Class with Automatic Properties/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Car_Class_with_Automatic_Properties;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
